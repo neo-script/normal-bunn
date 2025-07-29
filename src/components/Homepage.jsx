1260750906e2244e4f9e2de2948d035fb4fdc5cb
neo-script <zoesoyunlee07@gmail.com>
2025-07-29 10:19:12 +0900

    Create Homepage.jsx

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,11 @@
+function HomePage() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Bunn's website</h1>
+      <h2>click the buttons below</h2>
+      {/* etc... */}
+    </div>
+  );
+}
+
+export default HomePage;
